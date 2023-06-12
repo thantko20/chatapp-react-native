b18cbb6c1252e78df757c7c8ae0b7903595c8d76
Thant Ko Zaw <thantmkou10@gmail.com>
2023-06-12 11:22:09 +0630

    Add react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { View } from 'react-native';
 import Providers from './src/providers';
 import { Text } from './src/atoms';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/screens/login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Providers>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Providers>
   );
 }
---
+++ src/screens/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from '../atoms';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+};
+
+export default Login;
